56d9a2d8cbf60c3ea36d229bfec38ee03e94cbf2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-12-11 11:05:58 +0100

    Dodanie metody generycznej

---
+++ src/GenericTypes/LocalStorage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTypes;
+
+internal class LocalStorage
+{
+    private readonly IDictionary<string, string> _storage = new Dictionary<string, string>();
+
+    // Metoda generyczna 
+    // Szablon metody do której przekazujemy typ T
+    public void Set<T>(string key, T value)        
+    {
+        _storage.TryAdd(key, value.ToString());
+    }
+   
+    public T Get<T>(string key)
+    {
+        _storage.TryGetValue(key, out var value);
+
+        return (T)Convert.ChangeType(value, typeof(T));
+    }   
+}
+
+class Factory
+{
+    public static object Create(Type type)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static T Create<T>() { 
+        throw new NotImplementedException();
+    }
+}
--- src/GenericTypes/Program.cs
+++ src/GenericTypes/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GenericTypes;
+using GenericTypes.Model;
+
+Console.WriteLine("Hello, Generic Types!");
+
+LocalStorage localStorage = new LocalStorage();
+localStorage.Set("Key1", "Foo");
+localStorage.Set("temp", 21);
+
+string stringValue = localStorage.Get<string>("Key1");
+
+int intValue = localStorage.Get<int>("temp");
+
+
+Console.WriteLine(stringValue);
+Console.WriteLine(intValue);
+
+Customer customer = Factory.Create<Customer>();
