8a2048a4a7046b83a17999383189f8dc53c4cd66
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-12-11 10:29:05 +0100

    Dodanie przykładu zastowania komponentu generycznego w Blazor

---
+++ src/GenericTypes/BlazorApp/Model/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp.Model;
+
+
+public abstract class Base
+{
+
+}
+
+public abstract class BaseEntity : Base
+{
+    public int Id { get; set; }
+}
+
+public class Customer : BaseEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
