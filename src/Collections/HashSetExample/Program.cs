4544275907ab580ca2b2aea4b30550f88f4d25c7
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-12-12 10:56:54 +0100

    Dodanie przykładu zastosowania HashSet

--- src/Collections/HashSetExample/Program.cs
+++ src/Collections/HashSetExample/Program.cs
@@ -4,7 +4,12 @@ using Collections.Core;
 
 Console.WriteLine("Hello, HashSet!");
 
-TrackingServiceTest();
+// TrackingServiceTest();
+
+ImageServiceTest();
+
+Console.WriteLine();
+
 
 static void TrackingServiceTest()
 {
@@ -18,8 +23,8 @@ static void TrackingServiceTest()
         "192.168.0.2",
         "10.0.0.1",
         "192.168.0.3"
-    };    
-    
+    };
+
     foreach (string ipAddress in ipAddresses)
     {
         trackingService.TrackIP(ipAddress);
@@ -27,6 +32,17 @@ static void TrackingServiceTest()
 
     var uniqueIPs = trackingService.GetUniqueIPs();
 
-    uniqueIPs.Dump("List of Unique IP Addresses:");    
+    uniqueIPs.Dump("List of Unique IP Addresses:");
 }
 
+static void ImageServiceTest()
+{
+    ImageService imageService = new ImageService();
+    var image1 = new Image([1, 2, 3, 4]);
+    var image2 = new Image([1, 2, 3, 4, 5]);
+    var image3 = new Image([1, 2, 3, 4]);
+
+    imageService.TakeImage(image1);
+    imageService.TakeImage(image2);
+    imageService.TakeImage(image3);
+}
--- src/Collections/HashSetExample/TrackingService.cs
+++ src/Collections/HashSetExample/TrackingService.cs
@@ -1,15 +1,67 @@
-﻿namespace HashSetExample;
+﻿using System.Collections;
+
+namespace HashSetExample;
+
+class Image
+{
+    public byte[] Content { get; set; }
+
+    public override int GetHashCode()
+    {
+        return BitConverter.ToInt32(Content);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        Image otherImage = (Image)obj;
+
+        return StructuralComparisons.StructuralEqualityComparer.Equals(Content, otherImage.Content);
+    }
+
+    public Image(byte[] content)
+    {
+        Content = content;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.GetHashCode()}";
+    }
+}
+
+internal class ImageService
+{
+    private readonly HashSet<Image> _images = new HashSet<Image>();
+
+    public void TakeImage(Image image)
+    {
+        _images.Add(image);
+    }
+}
 
 internal class TrackingService
 {
+    // Złożoność obliczeniowa O(1)
+    private readonly HashSet<string> _uniqueIPs = new HashSet<string>();
+
     public void TrackIP(string ipAddress)
     {
-        throw new NotImplementedException();
+        if (_uniqueIPs.Add(ipAddress))
+        {
+            Console.WriteLine($"New IP Address {ipAddress}");
+        }
+        else
+        {
+            Console.WriteLine($"Duplicate IP Address {ipAddress}");
+        }
     }
 
     public IEnumerable<string> GetUniqueIPs()
     {
-        throw new NotImplementedException();
+        return _uniqueIPs;
     }
 
 
