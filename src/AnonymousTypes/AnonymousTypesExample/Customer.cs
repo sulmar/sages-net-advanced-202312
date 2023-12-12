4876ae8b25ffb3800a346d4d5caf0010c53ec8ba
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-12-12 13:56:53 +0100

    Dodanie przykładu zastosowania typu anonimowego

---
+++ src/AnonymousTypes/AnonymousTypesExample/Customer.cs
@@ -0,0 +1,44 @@
+﻿namespace AnonymousTypesExample;
+
+internal class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string HashedPassword { get; set; }
+    public bool IsRemoved { get; set; }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName} {Email} {IsRemoved}";
+    }
+}
+
+internal class CustomerInfo
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string City { get; private set; }
+
+    public CustomerInfo(Customer customer)
+    {
+        this.FirstName = customer.FirstName;
+        this.LastName = customer.LastName;
+        this.City = customer.City;
+    }
+}
+
+internal class CustomerInfo2
+{
+    public string Email { get; set; }
+    public bool IsRemoved { get; set; }
+
+    public CustomerInfo2(Customer customer)
+    {
+        this.Email = customer.Email;
+        this.IsRemoved = customer.IsRemoved;
+    }
+}
---
+++ src/AnonymousTypes/AnonymousTypesExample/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using AnonymousTypesExample;
+using Bogus;
+
+Console.WriteLine("Hello, Anonymous Types!");
+
+// Typ anonimowy
+var person = new
+{
+    FirstName = "John",
+    LastName = "Smith",
+    Age = 18
+};
+
+var customerFaker = new Faker<Customer>()
+    .RuleFor(p => p.Id, f => f.IndexFaker)
+    .RuleFor(p => p.FirstName, f => f.Person.FirstName)
+    .RuleFor(p => p.LastName, f => f.Person.LastName)
+    .RuleFor(p => p.Email, (f, customer) => $"{customer.FirstName}.{customer.LastName}@domain.com") // {firstname}.{lastname}@domain.com
+    .RuleFor(p => p.Address, f => f.Address.StreetAddress())
+    .RuleFor(p => p.City, f => f.Address.City())
+    .RuleFor(p => p.HashedPassword, f => f.Lorem.Word())
+    .RuleFor(p => p.IsRemoved, f => f.Random.Bool(0.3f))
+    ;
+
+var customers = customerFaker.GenerateLazy(100);
+
+var customerInfos = customers
+    .Select(customer => new { customer.FirstName, customer.LastName, customer.City} )
+    .ToList();
+
+foreach (var customer in customerInfos)
+{
+    Console.WriteLine($"{customer.FirstName} {customer.LastName} {customer.City}");
+}
