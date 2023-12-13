cf63182773471d85c1b8b68b34fcc4d217e0fd0e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-12-13 17:35:34 +0100

    Dodanie nowości z .NET 8

---
+++ src/NET8Examples/Customer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace NET8Examples;
+
+// Immutable (niezmienny)
+record Vehicle([Description] string Model, decimal Price, string Vin);
+
+class Vehicle2
+{
+    public string Model { get; init; }
+    public decimal Price { get; init; }
+    public string Vin { get; init; }
+
+    public Vehicle2(string model, decimal price, string vin)
+    {
+        Model = model;
+        Price = price;
+        Vin = vin;
+    }
+}
+
+internal record VipCustomer : Customer
+{
+    public string Class { get; set; }
+}
+
+// Primary Constructors
+class House(string name, string? flatNumber = null, decimal? price = null)
+{
+}
+
+class Person
+{
+    required
+    public string FirstName { get; set; }
+
+    public string? SecondName { get; set; }
+
+    required
+    public string LastName { get; set; }
+}
+
+
+// Krotki
+internal record Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+
+    required
+    public string LastName { get; set; }
+
+    public void DoWork()
+    {
+
+    }
+
+    //public override bool Equals(object? obj)
+    //{
+    //    if (obj == null || GetType() != obj.GetType())
+    //        return false;
+
+    //    Customer otherCustomer = (Customer)obj;
+
+    //    return StructuralComparisons.StructuralEqualityComparer.Equals(Id, otherCustomer.Id) 
+    //        && StructuralComparisons.StructuralEqualityComparer.Equals(FirstName, otherCustomer.FirstName)
+    //        && StructuralComparisons.StructuralEqualityComparer.Equals(LastName, otherCustomer.LastName);
+    //}
+
+    //public override int GetHashCode()
+    //{
+    //    return base.GetHashCode();
+    //}
+
+}
---
+++ src/NET8Examples/Program.cs
@@ -0,0 +1,28 @@
+﻿using NET8Examples;
+
+Console.WriteLine("Hello, News!");
+
+Person person = new Person { FirstName = "John", LastName = "Smith" };
+
+IEnumerable<Vehicle> vehicles = new List<Vehicle>();
+
+House house = new House("home");
+
+Vehicle vehicle = new Vehicle("Mazda", 10000, "1234");
+
+
+var source1Customer = new Customer { Id = 1, FirstName = "John", LastName = "Smith" };
+
+var source2Customer = new Customer { Id = 1, FirstName = "John", LastName = "Smith" };
+
+source1Customer.LastName = "Spider";
+
+if (source1Customer.Equals(source2Customer))
+{
+    Console.WriteLine("The same customer!");
+}
+else
+{
+    Console.WriteLine("Different customer!");
+}
+
