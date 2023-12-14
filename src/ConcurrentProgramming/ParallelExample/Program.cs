df786237911090aaebb33a924328841c1dc8d061
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-12-14 17:00:54 +0100

    Dodanie przykladow przetwarzania rownoleglego

--- src/ConcurrentProgramming/ParallelExample/Program.cs
+++ src/ConcurrentProgramming/ParallelExample/Program.cs
@@ -1,2 +1,10 @@
 ﻿
+using System.Collections.Generic;
+
 Console.WriteLine("Hello, Parallel!");
+
+var numbers = Enumerable.Range(0, 10000).ToList();
+
+
+Parallel.ForEach(numbers, number => Console.WriteLine(number));
+
--- src/ConcurrentProgramming/ParallelLinqExample/Program.cs
+++ src/ConcurrentProgramming/ParallelLinqExample/Program.cs
@@ -1,2 +1,58 @@
 ﻿
+
 Console.WriteLine("Hello, PLINQ!");
+
+// https://learn.microsoft.com/pl-pl/dotnet/standard/parallel-programming/introduction-to-plinq
+
+Console.WriteLine("LINQ");
+
+var result = "abcdef".ToCharArray()
+    .Select(c => char.ToUpper(c));
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("Parallel LINQ");
+
+var source = Enumerable.Range(1, 10000);
+var query = from item in source.AsParallel().WithDegreeOfParallelism(2)
+            where Compute(item) > 42
+            select item;
+
+foreach (var item in query)
+{
+    Console.WriteLine(item);
+}
+
+
+Console.ReadKey();
+
+
+int Compute(int item)
+{
+   
+    Thread.Sleep(100);
+   
+    Console.Write(".");
+
+    return item;
+}
+
+
+return;
+
+while (true)
+
+{
+    "abcdef".AsParallel()
+        .Select(item => char.ToUpper(item))
+        .ForAll(item => Console.WriteLine(item));
+
+
+    Console.WriteLine("---");
+
+    Thread.Sleep(1000);
+
+}
