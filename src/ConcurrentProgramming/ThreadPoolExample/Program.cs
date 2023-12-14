fd2773a099bbd3b3c5f60cc4ea1372de48677244
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-12-14 10:34:42 +0100

    Dodanie przykładu zastosowania puli wątków

--- src/ConcurrentProgramming/ThreadPoolExample/Program.cs
+++ src/ConcurrentProgramming/ThreadPoolExample/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, Thread Pool!");
+﻿using ThreadPoolExample;
+
+Console.WriteLine("Hello, Thread Pool!");
+
+MailingService mailingService = new MailingService();
+
+ThreadPool.GetAvailableThreads(out int workerThreads, out int workerCount);
+
+Console.WriteLine($"{workerThreads} {workerCount}");
+
+// bool s = ThreadPool.SetMaxThreads(1, 1);
+
+for (int i = 0; i < 100; i++)
+{
+    ThreadPool.QueueUserWorkItem(mailingService.SendToObject, $"john{i}@domain.com");
+
+    Thread.Sleep(500);
+
+}
+
+Console.WriteLine("Done.");
+
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadLine();
